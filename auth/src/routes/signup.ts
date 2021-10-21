94e95f18cb9b1b5c7e2e59533e78d0a02b833393
Tony Proenca <antonio.proenca@justeattakeaway.com>
2021-10-21 21:28:18 +0200

    Implementing User domain module.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,32 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
+
+async function validateAlreadyExistingUser(email: string) {
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw new BadRequestError("Email in use");
+  }
+}
+
+async function saveUser(email: string, password: string) {
+  const user = User.build({ email, password });
+  await user.save();
+  return user;
+}
+
+function validateRequest(req: Request) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
+}
 
 router.post(
   "/api/users/signup",
@@ -14,19 +37,12 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters."),
   ],
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
+  async (req: Request, res: Response) => {
+    validateRequest(req);
     const { email, password } = req.body;
-
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
-
-    res.send({});
+    await validateAlreadyExistingUser(email);
+    const user = await saveUser(email, password);
+    res.status(201).send(user);
   }
 );
 
